56d41107bc6929392475e3eb1a1c722ac2568d1a
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-07-10 13:44:22 -0600

    spacing and some js

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log("Juego de la loteria");
+
+//All the cards that will be used
+const loteriaCards = [
+	"El Uber", 
+	"La Notificacion",
+	"El Twitter",
+	"El Fidget Spinner",
+	"El Troll",
+	"La Feminist",
+	"La Selfie",
+	"La Thigh Gap",
+	"El Face Swap",
+	"El Global Warming",
+	"El Hipster",
+	"La Flower Crown",
+	"El Tinder",
+	"El Nerd",
+	"El Dj",
+]
+
+//
+const player = {
+	playerCards: [],
+	cardsMatched: [],
+	score: 0,
+	level: 1,
+}
+
+//randomize cards in the buttons 
+// function randomizeCards 
